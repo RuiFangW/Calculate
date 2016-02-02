fd1d02c3eb92258b3ec022eaab0e2abf7d788b7e
RuiFangW <724266891@qq.com>
2016-02-02 16:26:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo2/calculate/Calculate.java
@@ -0,0 +1,12 @@
+package com.example.lenovo2.calculate;
+
+import android.app.Activity;
+
+/**
+ * Calculate
+ *
+ * @author: lenovo
+ * @time: 2016/2/2 15:42
+ */
+public class Calculate extends Activity {
+}
